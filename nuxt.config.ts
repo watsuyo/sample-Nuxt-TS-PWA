d2269682077b123cd8e0480ac7d2db5fbc6fb145
Tsuyoshi Hirosawa <dmwtpdmwtp24@gmail.com>
2019-04-23 12:08:47 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,62 @@
+const pkg = require('./package')
+import NuxtConfiguration from '@nuxt/config'
+
+const nuxtConfig: NuxtConfiguration = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Alice',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  bnuild: {
+    extend(config: any, ctx: any) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
+export default nuxtConfig
