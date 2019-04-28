22d726c8a88c0318f8b585ee021ed549d4ea66a5
Tsuyoshi Hirosawa <dmwtpdmwtp24@gmail.com>
2019-04-28 13:21:21 +0900

    conponentを分ける

---
+++ components/presentations/CosmeSummary.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>イエベ春</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CosmeDetail extends Vue {}
+</script>
